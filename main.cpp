ce7a49cc87c080f67e0a37a47da4beaa68c9fd32
gavind20 <gjdacumos@mail.csuchico.edu>
2018-09-19 17:50:34 -0700

    added everything

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
-
+    string firstname;
+    string lastname;
   //get user input
-
+    cout<<"What is your first name?\n"<<endl;
+    cin>>firstname;
+    cout<<"What is your last name?\n"<<endl;
+    cin>>lastname;
   //tell fortune
-
+    cout<<"Welcome, "<<firstname[0]<<lastname[0];
+    cout<<", here is your fortune..."<<endl;
+    cout<<"your lucky number is "<<firstname.length()<<" letters"<<endl;
+    
   return 0;
 }
